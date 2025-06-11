9945cf9c7e99f7a6bce739acac94802a1277b51f
KuzmitchKalashnikov <Kalashnikovivan18@mail.ru>
2025-06-11 20:58:42 +0300

    Update apps.py

--- apps.py
+++ apps.py
@@ -1,6 +1,6 @@
-from flask.apps import AppConfig
+from django.apps import AppConfig
 
 
 class PlantAppConfig(AppConfig):
-    default_auto_field = 'flask.db.models.BigAutoField'
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'plant_app'
