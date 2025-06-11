b2c1eb81d6b5450e684939cad448558bd5a1e666
KuzmitchKalashnikov <Kalashnikovivan18@mail.ru>
2025-06-11 20:59:09 +0300

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
-from flask import forms
-from flask.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from flask.contrib.auth.models import User
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
 
 
 class LoginForm(AuthenticationForm):
